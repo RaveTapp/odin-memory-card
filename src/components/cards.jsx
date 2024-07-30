e38242dec12f4173547365ba534507c08832c982
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-07-30 14:27:06 +0200

    Add basic display of cards

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,32 +1,52 @@
-import shuffle from "array-permutation/lib/shuffle"
-import cat from "../gifs/cat.gif"
-import dinosaur from "../gifs/dinosaur.gif"
-import dog from "../gifs/dog.gif"
-import elephant from "../gifs/elephant.gif"
-import fox from "../gifs/fox.gif"
-import giraffe from "../gifs/giraffe.gif"
-import gorilla from "../gifs/gorilla.gif"
-import horse from "../gifs/horse.gif"
-import kangaroo from "../gifs/kangaroo.gif"
-import ostrich from "../gifs/ostrich.gif"
-import tiger from "../gifs/tiger.gif"
-import turtle from "../gifs/turtle.gif"
+import shuffle from "array-permutation/lib/shuffle";
 
-import {shuffle} from "array-permutation"
+import cat from "../gifs/cat.gif";
+import dinosaur from "../gifs/dinosaur.gif";
+import dog from "../gifs/dog.gif";
+import elephant from "../gifs/elephant.gif";
+import fox from "../gifs/fox.gif";
+import giraffe from "../gifs/giraffe.gif";
+import gorilla from "../gifs/gorilla.gif";
+import horse from "../gifs/horse.gif";
+import kangaroo from "../gifs/kangaroo.gif";
+import ostrich from "../gifs/ostrich.gif";
+import tiger from "../gifs/tiger.gif";
+import turtle from "../gifs/turtle.gif";
 
-const animalGifs = [cat, dinosaur, dog, elephant, fox, giraffe, gorilla, horse, kangaroo, ostrich, tiger, turtle];
+import "array-permutation";
 
-export function CardContainer(){
-    console.log(animalGifs);
-    shuffle(animalGifs);
-    console.log(animalGifs);
+const animalGifs = [
+  { gif: cat, name: "Cat" },
+  { gif: dinosaur, name: "Dinosaur" },
+  { gif: dog, name: "Dog" },
+  { gif: elephant, name: "Elephant" },
+  { gif: fox, name: "Fox" },
+  { gif: giraffe, name: "Giraffe" },
+  { gif: gorilla, name: "Gorilla" },
+  { gif: horse, name: "Horse" },
+  { gif: kangaroo, name: "Kangaroo" },
+  { gif: ostrich, name: "Ostrich" },
+  { gif: tiger, name: "Tiger" },
+  { gif: turtle, name: "Turtle" },
+];
 
-    return <Card></Card>
+export function CardContainer() {
+  shuffle(animalGifs);
+  const cards = [];
+  for (let i = 0; i < animalGifs.length; i++) {
+    cards.push(
+      <Card key={i} gif={animalGifs[i].gif} name={animalGifs[i].name} />
+    );
+  }
+
+  return <>{cards}</>;
 }
 
-function Card(){
-    return <div className="card">
-        <img src={ostrich} alt="a" />
-        <h2>Card name</h2>
+function Card({gif, name}) {
+  return (
+    <div className="card">
+      <img src={gif} alt={name} />
+      <h2>{name}</h2>
     </div>
+  );
 }
