6dff1ff8f9c14c20157b1448e2afb4ab60600b84
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-07-30 11:57:44 +0200

    Setup project for React

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./styles/main.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h1>Test</h1>
+  </React.StrictMode>
+);
